0932a87e8ea8cb27c6602f945faf9bfd58ec5d70
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-12 16:39:58 +0930

    Added a session_helper.php and initialised in the bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,10 +2,8 @@
 // Load Config
 require_once 'config/config.php';
 
-// Load Libraries
-// require_once 'lib/Core.php';
-// require_once 'lib/Controller.php';
-// require_once 'lib/Database.php';
+// Load Helpers
+require_once 'helpers/session_helper.php';
 
 // Auto-load Core Libraries
 spl_autoload_register(function ($className) {
