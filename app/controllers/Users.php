c288910e88674f685b1f593f823f26aa9937d7f8
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-12 18:05:20 +0930

    Added comments to User controller and changed name of a method

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -5,18 +5,18 @@ class Users extends Controller
 
     public function __construct()
     {
+        // Instantiate the user model to enable data grab from database
         $this->userModel = $this->model('User');
     }
 
     // Methods
-
     public function register()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $data = $this->initUserData();
             $data = $this->validateRegistrationData($data);
 
-            if ($this->noErrors($data)) {
+            if ($this->noErrorsCheck($data)) {
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
                 if ($this->userModel->register($data)) {
                     redirectURL('pages/index');
@@ -36,6 +36,7 @@ class Users extends Controller
 
     private function initUserData()
     {
+        // Initialise the data needed for view
         return [
             'name' => '',
             'email' => '',
@@ -51,6 +52,7 @@ class Users extends Controller
 
     private function validateRegistrationData($data)
     {
+        // Sanitise the POST input
         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
         // Trim and store sanitized input
@@ -58,11 +60,14 @@ class Users extends Controller
         $data['email'] = trim($_POST['email']);
         $data['password'] = trim($_POST['password']);
         $data['confirm_password'] = trim($_POST['confirm_password']);
+
         // Validation logic
+        // Check if empty name input
         if (empty($data['name'])) {
             $data['name_error'] = 'Please enter a name';
         }
 
+        // Check if empty email input
         if (empty($data['email'])) {
             $data['email_error'] = 'Please enter an email';
         } else {
@@ -72,12 +77,14 @@ class Users extends Controller
             }
         }
 
+        // Check if empty password input and meets length requirement
         if (empty($data['password'])) {
             $data['password_error'] = 'Please enter a password';
         } elseif (strlen($data['password']) < 6) {
             $data['password_error'] = 'Please enter password with more than six characters';
         }
 
+        // Check if empty confirm password and passwords input match
         if (empty($data['confirm_password'])) {
             $data['confirm_password_error'] = 'Please confirm the password';
         } elseif ($data['password'] !== $data['confirm_password']) {
@@ -87,8 +94,9 @@ class Users extends Controller
         return $data;
     }
 
-    private function noErrors($data)
+    private function noErrorsCheck($data)
     {
+        // Check if there are any input errors
         return (empty($data['name_error']) &&
             empty($data['email_error']) &&
             empty($data['password_error']) &&
