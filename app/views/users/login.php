0c1ef10712b2be25f666a8d67f5c6e46ed460117
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-13 15:40:00 +0930

    Changed the way on how error messages popup in login.php view

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -7,17 +7,15 @@
                 <h2 class="text-center">Sign In</h2>
                 <p class="text-center mb-4">Please fill out this form to login</p>
                 <form action="<?php echo URLROOT; ?>/users/login" method="POST">
+                    <?php if (!empty($data['login_error'])) : ?> <div class="alert alert-danger" role="alert"><?php echo $data['login_error'] ?></div>
+                    <?php endif ?>
                     <div class="form-floating mb-2">
                         <input type="email" name="email" placeholder="Place password here" value="<?php echo $data['email'] ?>" class="form-control <?php echo (!empty($data['login_error'])) ? 'is-invalid' : ''; ?>">
                         <label for="email" class="form-label">Email</label>
-                        <?php if (!empty($data['login_error'])) : ?><span class="invalid-feedback"><?php echo $data['login_error'] ?></span>
-                        <?php endif ?>
                     </div>
                     <div class="form-floating mb-2">
                         <input type="password" name="password" placeholder="Place password here" value="<?php echo $data['password'] ?>" class="form-control <?php echo (!empty($data['login_error'])) ? 'is-invalid' : ''; ?>">
                         <label for="password" class="form-label">Password</label>
-                        <?php if (!empty($data['login_error'])) : ?><span class="invalid-feedback"><?php echo $data['login_error'] ?></span>
-                        <?php endif ?>
                     </div>
                     <div class="row">
                         <div class="col-12 d-flex justify-content-center mt-2">
