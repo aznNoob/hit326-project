bf42b44c97b44ea6dbb3bf5c948ae65b9ba43ba3
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-14 02:09:48 +0930

    Changed getArticles list order to descending by date

--- app/models/Article.php
+++ app/models/Article.php
@@ -11,14 +11,14 @@ class Article
 
     public function getArticles()
     {
-        $this->db->query('SELECT articles.id, articles.title, articles.created_at, articles.body, users.name FROM articles, users WHERE articles.user_id = users.id;');
+        $this->db->query('SELECT articles.id, articles.title, articles.created_at, articles.body, users.name FROM articles, users WHERE articles.user_id = users.id ORDER BY articles.created_at DESC;');
         $articleResults = $this->db->resultSet();
         return $articleResults;
     }
 
     public function addArticle($data)
     {
-        $this->db->query('INSERT INTO posts(user_id, title, body) VALUES(:user_id, :title, :body)');
+        $this->db->query('INSERT INTO articles(user_id, title, body) VALUES(:user_id, :title, :body)');
         $this->db->bind(':user_id', $data['user_id']);
         $this->db->bind(':title', $data['title']);
         $this->db->bind(':body', $data['body']);
