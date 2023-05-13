fa22b2a30606ef222ba90f827838f82d755bb3f6
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-13 13:41:49 +0930

    Tweaked the displaying of errors in register.php view

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -15,17 +15,20 @@
                     <div class="form-floating mb-2">
                         <input type="email" name="email" placeholder="Place email here" value="<?php echo $data['email'] ?>" class="form-control form-control-lg <?php echo (!empty($data['email_error'])) ? 'is-invalid' : ''; ?>">
                         <label for="email" class="form-label">Email</label>
-                        <span class="invalid-feedback"><?php echo $data['email_error'] ?></span>
+                        <?php if (!empty($data['email_error'])) : ?><span class="invalid-feedback"><?php echo $data['email_error'] ?></span>
+                        <?php endif ?>
                     </div>
                     <div class="form-floating mb-2">
                         <input type="password" name="password" placeholder="Place password here" value="<?php echo $data['password'] ?>" class="form-control form-control-lg <?php echo (!empty($data['password_error'])) ? 'is-invalid' : ''; ?>">
                         <label for="password" class="form-label">Password</label>
-                        <span class="invalid-feedback"><?php echo $data['password_error'] ?></span>
+                        <?php if (!empty($data['password_error'])) : ?><span class="invalid-feedback"><?php echo $data['password_error'] ?></span>
+                        <?php endif ?>
                     </div>
                     <div class="form-floating mb-2">
                         <input type="password" name="confirm_password" placeholder="Place confirm password here" value="<?php echo $data['confirm_password'] ?>" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_error'])) ? 'is-invalid' : ''; ?>">
                         <label for="confirm_password" class="form-label">Confirm Password</label>
-                        <span class="invalid-feedback"><?php echo $data['confirm_password_error'] ?></span>
+                        <?php if (!empty($data['confirm_password_error'])) : ?><span class="invalid-feedback"><?php echo $data['confirm_password_error'] ?></span>
+                        <?php endif ?>
                     </div>
 
                     <div class="row">
