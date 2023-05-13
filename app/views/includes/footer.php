1070059fbafc8e83f819ec7b01956b41b0fd237c
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-13 13:45:01 +0930

    Made footer slighty bigger

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -2,11 +2,9 @@
 </body>
 
 <footer class="fixed-bottom bg-light text-center text-lg-start mt-5">
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.025);">
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
         © 2023 Copyright: Group 12
     </div>
-    <!-- Copyright -->
 </footer>
 
 </html>
