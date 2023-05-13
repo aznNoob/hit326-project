d883ebe6929e91bc1c942b4d4c857ccabaa77e07
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-13 22:31:23 +0930

    Created Articles.php controller and Article.php model with the construct methods in both

---
+++ app/controllers/Articles.php
@@ -0,0 +1,15 @@
+<?php
+
+class Articles extends Controller
+{
+    private $articleModel;
+
+    public function __construct()
+    {
+        $this->articleModel;
+    }
+
+    public function index()
+    {
+    }
+}
