b69915e6656019fc0baad3d8529833b01ae2d6a1
ANDREW MU <andrew.harambe@hotmail.com>
2023-05-14 23:48:26 +0930

    Instantiate Article model and update the index method in Pages controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,13 +2,20 @@
 class Pages extends Controller
 {
 
+    private $articleModel;
+
     public function __construct()
     {
+        $this->articleModel = $this->model('Article');
     }
 
     public function index()
     {
-        $this->view('index');
+        $topArticles = $this->articleModel->getSixLatestArticles();
+        $data = [
+            'articles' => $topArticles
+        ];
+        $this->view('index', $data);
     }
 
     public function about()
--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -15,26 +15,3 @@ function userHasRole($role)
 {
     return (isset($_SESSION['user_role']) && $_SESSION['user_role'] == $role);
 }
-
-function displayDate($date)
-{
-    $now = new DateTime();
-    $inputDate = new DateTime($date);
-    $interval = $now->diff($inputDate);
-
-    $days = $interval->days;
-    $months = $interval->m;
-    $years = $interval->y;
-
-    if ($years > 0) {
-        return $years . ' year' . ($years > 1 ? 's' : '') . ' ago';
-    } elseif ($months > 0) {
-        return $months . ' month' . ($months > 1 ? 's' : '') . ' ago';
-    } elseif ($days > 1) {
-        return $days . ' days ago';
-    } elseif ($days == 1) {
-        return 'Yesterday';
-    } else {
-        return 'Today';
-    }
-}
